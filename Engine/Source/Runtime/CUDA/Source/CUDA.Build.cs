f43a65f5baa4069eb6a8570d8aee068e8449cc33
Denis.b <denis.b@rznet.com>
2021-10-11 14:40:18 +0700

    ++ThirdPArty Nvidia CUDA

---
+++ Engine/Source/Runtime/CUDA/Source/CUDA.Build.cs
@@ -0,0 +1,44 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using System;
+using System.IO;
+using UnrealBuildTool;
+using System.Globalization;
+//using Tools.DotNETCommon;
+using EpicGames.Core;
+
+public class CUDA : ModuleRules
+{
+	public CUDA(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PrivateDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"RenderCore",
+			"RHI",
+			"Engine",
+		});
+		
+		PublicDependencyModuleNames.Add("CUDAHeader");
+
+		if (Target.IsInPlatformGroup(UnrealPlatformGroup.Windows) || Target.IsInPlatformGroup(UnrealPlatformGroup.Unix))
+		{		
+			var EngineDir = Path.GetFullPath(Target.RelativeEnginePath);
+
+			AddEngineThirdPartyPrivateStaticDependencies(Target, "NVAftermath");
+			
+			PrivateIncludePathModuleNames.Add("VulkanRHI");
+			PrivateIncludePaths.Add(Path.Combine(EngineDir, "Source/Runtime/VulkanRHI/Private"));	
+
+			if (Target.IsInPlatformGroup(UnrealPlatformGroup.Unix))
+			{
+				PrivateIncludePaths.Add(Path.Combine(EngineDir, "Source/Runtime/VulkanRHI/Private/Linux"));	
+			} 
+			else if (Target.Platform == UnrealTargetPlatform.Win64)
+			{
+				PrivateIncludePaths.Add(Path.Combine(EngineDir, "Source/Runtime/VulkanRHI/Private/Windows"));	
+			}
+
+			AddEngineThirdPartyPrivateStaticDependencies(Target, "Vulkan");
+		}
+	}
+}
